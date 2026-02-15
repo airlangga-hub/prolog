f51da972b20814579fc83c32b0dd642b9e0b5a4a
Airlangga <airlanggakrishna@gmail.com>
2026-02-16 01:28:50 +0700

    chore: test helpers

--- internal/log/store_test.go
+++ internal/log/store_test.go
@@ -29,3 +29,44 @@ func TestStoreAppendRead(t *testing.T) {
 	
 	testRead(t, s)
 }
+
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+		require.Equal(t, pos+n, width*i)
+	}
+}
+
+func testRead(t *testing.T, s *store) {
+	t.Helper()
+	var pos uint64
+	for i := uint64(1); i < 4; i++ {
+		read, err := s.Read(pos)
+		require.NoError(t, err)
+		require.Equal(t, read, write)
+		pos += width
+	}
+}
+
+func testReadAt(t *testing.T, s *store) {
+	t.Helper()
+	for i, off := uint64(1), int64(0); i < 4; i++ {
+		b := make([]byte, lenWidth)
+		n, err := s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, n, lenWidth)
+		
+		off += int64(n)
+		
+		size := enc.Uint64(b)
+		b = make([]byte, size)
+		n, err = s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, write, b)
+		require.Equal(t, size, n)
+		
+		off += int64(n)
+	}
+}
